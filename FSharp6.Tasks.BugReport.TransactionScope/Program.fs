8ce5846b0ad386173cdb750a6bb0fe9bb554784e
max <grebeniukmax@gmail.com>
2021-12-31 00:33:34 +0100

    CHK: clean reproduce of transaction scope issue

--- FSharp6.Tasks.BugReport.TransactionScope/Program.fs
+++ FSharp6.Tasks.BugReport.TransactionScope/Program.fs
@@ -1,15 +1,69 @@
 module Main
 
-open FSharp6.Tasks.BugReport.TransactionScope
+open System.Transactions
+open Npgsql
+open Net6.TaskResult
+
+module Db =
+    let connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=postgres;Pooling=true"
+    
+    let openConnectionAsync() = 
+        task {
+            let conn = new NpgsqlConnection(connectionString)
+            do! conn.OpenAsync(Async.DefaultCancellationToken)
+            return conn
+        }
+    
+    let createTransactionScope () =
+        let opt = TransactionOptions(IsolationLevel=IsolationLevel.ReadCommitted, Timeout=TransactionManager.MaximumTimeout)
+        new TransactionScope(TransactionScopeOption.Required, opt, TransactionScopeAsyncFlowOption.Enabled)
+       
+    let logCurrentCurrentTransactionId (context : string) =
+        task {
+            use! connection = openConnectionAsync()
+            use cmd = connection.CreateCommand()
+            cmd.CommandText <- "SELECT txid_current() AS transaction_id;"
+            use! reader = cmd.ExecuteReaderAsync()
+            let! _more = reader.ReadAsync()
+            let transactionId = reader.GetInt64(0)
+            printfn $"%s{context}: transaction_id=%i{transactionId}."
+        }
+
+let performScopedWork context =
+    // TODO: change to taskResult here to change transaction scope behavior
+    taskResult {
+        use scope = Db.createTransactionScope()
+        do! Db.logCurrentCurrentTransactionId $"%s{context}"
+        scope.Complete()
+    }
+
+let scope3 () =
+    taskResult {
+        use scope = Db.createTransactionScope()
+        do! performScopedWork "scope3"
+        scope.Complete()
+    }
+    
+
+let scope2 () =
+    taskResult {
+        use scope = Db.createTransactionScope()
+        do! performScopedWork "scope2"
+        do! scope3 ()
+        scope.Complete()
+    }
+    
+
+let scope1 () =
+    taskResult {
+        use scope = Db.createTransactionScope()
+        do! performScopedWork "scope1"
+        do! scope2()
+        scope.Complete()
+    }
 
 [<EntryPoint>]
 let main _argv =
     printfn "Native task computation."
-    Native.scope1().GetAwaiter().GetResult()
-    
-    printfn "Ply task computation."
-    Ply.scope1().GetAwaiter().GetResult()
-    
-    printfn "C# task."
-    CSharp.TransactionFlow.Perform().GetAwaiter().GetResult()
+    let asdas = scope1().GetAwaiter().GetResult()
     0
---
+++ FSharp6.Tasks.BugReport.TransactionScope/TaskResult.fs
@@ -0,0 +1,55 @@
+﻿module Net6.TaskResult
+
+open System
+open System.Threading.Tasks
+
+type TaskResultBuilder() =
+
+    /// <summary>
+    /// Method lets us transform data types into our internal representation. This is the identity method to recognize the self type.
+    ///
+    /// See https://stackoverflow.com/questions/35286541/why-would-you-use-builder-source-in-a-custom-computation-expression-builder
+    /// </summary>
+    member inline _.Source(task : Task<Result<'a, 'e>>) : Task<Result<'a, 'e>> =
+        task
+
+    member inline _.Zero() : Task<Result<unit, 'e>> =
+        () |> Result.Ok |> Task.FromResult
+
+    member inline _.Bind(taskResult : Task<Result<'a, 'e>>, binder : 'a -> Task<Result<'b, 'e>>) : Task<Result<'b, 'e>> =
+        task {
+            match! taskResult with
+            | Error e ->
+                return Error e
+            | Ok d ->
+                return! binder d
+        }
+
+    member inline _.Delay(generator : unit -> Task<Result<'a, 'e>>) : unit -> Task<Result<'a, 'e>> =
+        generator
+
+    member inline _.Using(resource : 'a :> IDisposable, binder : 'a -> Task<Result<'b, 'e>>) : Task<Result<'b, 'e>> =
+        task {
+            //TODO: change this to use - net6 will result in IAsyncDisposable compile time error
+            let res = resource
+            try
+                let! result = binder res
+                return result
+            finally
+                res.Dispose()
+        }
+        
+    member inline _.Run(f : unit -> Task<'m>) = f ()
+    
+let taskResult = TaskResultBuilder()
+
+// Having members as extensions gives them lower priority in
+// overload resolution between Task<'a> and Task<Either<'a>>.
+[<AutoOpen>]
+module TaskResultCEExtensions =
+    type TaskResultBuilder with
+        member inline _.Source(source : Task<'a>) : Task<Result<'a, 'e>> =
+            task {
+                let! s = source
+                return Result.Ok s
+            }
